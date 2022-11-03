a2aba8d9de9cfdaace2d0f5d8ab3c034a2098e22
Hariharan S <93510043+hari0805@users.noreply.github.com>
2022-11-03 16:27:28 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -108,7 +108,7 @@ def home(request):
 @login_required(login_url='login')
 # @allowed_users(allowed_roles=['customer', 'admin'])
 def accountSettings(request):
-	customer = Customer.objects.get(name=request.user)
+	customer = request.user.customer # Customer.objects.get(name=request.user)
 	form = CustomerForm(instance=customer)
 
 	if request.method == 'POST':
