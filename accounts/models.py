5c9ca135472ec33c7970f68ec661f0ef0bf449b5
Hariharan S <93510043+hari0805@users.noreply.github.com>
2022-11-09 11:14:37 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create your models here.
 
 class Customer(models.Model):
-	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, null=True, blank=True, related_name='customer', on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.EmailField(max_length=200, null=True)
